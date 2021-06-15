c111bd7f1ff71364fd64406ed2854ac03a7ce28c
balamurugan.thirumalaikumar <balamurugan.thirumalaikumar@syncfusion.com>
2021-06-15 12:31:41 +0530

    AddSample_WF-60136

---
+++ GridGroupingWF_CS/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace CellGrid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gridControl1 = new Syncfusion.Windows.Forms.Grid.GridControl();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControl1.Location = new System.Drawing.Point(12, 24);
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.SerializeCellsBehavior = Syncfusion.Windows.Forms.Grid.GridSerializeCellsBehavior.SerializeAsRangeStylesIntoCode;
+            this.gridControl1.Size = new System.Drawing.Size(376, 209);
+            this.gridControl1.SmartSizeBox = false;
+            this.gridControl1.TabIndex = 0;
+            this.gridControl1.Text = "gridControl1";
+            this.gridControl1.UseRightToLeftCompatibleTextBox = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 257);
+            this.Controls.Add(this.gridControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "PushButton-Cell click";
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+
+        #endregion
+
+        private Syncfusion.Windows.Forms.Grid.GridControl gridControl1;
+
+    }
+}
+
